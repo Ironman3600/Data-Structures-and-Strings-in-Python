3036207e28fd7c46df265d6942f3d2bd108a036f
uday kumar <157950579+Ironman3600@users.noreply.github.com>
2025-08-19 21:08:47 +0530

    Add files via upload

---
+++ Demonstrate List Slicing.py
@@ -0,0 +1,13 @@
+# dictionary = {'a':'apple','b':'ball','c':'cat'}
+# dictionary['a'] = 'elephant'
+#
+# print(dictionary.get('g',"'g' not found"))
+
+original_list = list(range(1, 11))
+print("original list: ",original_list)
+b=list(range(1,6))
+c = b[::-1]
+
+print("Extracted first five elements: ",b)
+print("Reversed extracted elements: ",c)
+
