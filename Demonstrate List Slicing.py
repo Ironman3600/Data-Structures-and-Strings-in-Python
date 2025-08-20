9fade26e504f3c042f06bdbe5ec6e0ba67a87257
uday kumar <157950579+Ironman3600@users.noreply.github.com>
2025-08-21 00:28:58 +0530

    Update Demonstrate List Slicing.py

--- Demonstrate List Slicing.py
+++ Demonstrate List Slicing.py
@@ -1,7 +1,3 @@
-# dictionary = {'a':'apple','b':'ball','c':'cat'}
-# dictionary['a'] = 'elephant'
-#
-# print(dictionary.get('g',"'g' not found"))
 
 original_list = list(range(1, 11))
 print("original list: ",original_list)
@@ -10,4 +6,5 @@ c = b[::-1]
 
 print("Extracted first five elements: ",b)
 print("Reversed extracted elements: ",c)
+
 
